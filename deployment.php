26e83ba921ac3771e385f932a6dbd670575c6192
chandanatn11 <134474988+chandanatn11@users.noreply.github.com>
2024-03-14 11:30:53 +0530

    Add files via upload

---
+++ deployment.php
@@ -0,0 +1,11 @@
+<?php
+
+// Sample PHP script to display a welcoming message
+
+$name = "Jenkins";
+
+echo "Welcome to our PHP project, $name!";
+echo "\n";
+echo "This is just a simple demonstration.";
+
+?>
